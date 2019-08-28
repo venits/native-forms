d5b96ef10e061be9d21de2db16e3a01149c376cc
Venits <tomasz.przybyl.it@gmail.com>
2019-08-28 13:55:02 +0200

    Init

---
+++ App.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import NativeForms from 'native-forms';
+
+const App = () => {
+  const [hasForm, showForm] = useState(false);
+  const show = () => showForm(true);
+  const hide = () => showForm(false);
+
+  return (
+    <View style={styles.container}>
+      <Text>NativeForms.com</Text>
+
+      <Button title="Show Form" onPress={show} />
+
+      {hasForm && (
+        <NativeForms
+          form="https://my.nativeforms.com/sWO3UzYt0mcvZWPmZyZkRkZxUXTy0Db"
+          onClose={hide}
+        />
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
